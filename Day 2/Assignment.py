7651196551ec94532e2c74ba7875cf2a59bd4a7e
Pragati Kumari <44642938+mepragati@users.noreply.github.com>
2020-07-23 22:35:40 +0530

    Add files via upload

---
+++ Day 2/Assignment.py
@@ -0,0 +1,27 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 22:39:24) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=7
+>>> type(a)
+<class 'int'>
+>>> b=87.656
+>>> type(b)
+<class 'float'>
+>>> c=True
+>>> type(c)
+<class 'bool'>
+>>> b>a
+True
+>>> a<=4
+False
+>>> b,a=6,8.6
+>>> b
+6
+>>> a
+8.6
+>>> a==b
+False
+>>> a==8.6 and b>8
+False
+>>> a==8.6 or b>8
+True
+>>> 
